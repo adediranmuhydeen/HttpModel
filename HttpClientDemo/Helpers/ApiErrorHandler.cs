057227ed004264062387515a3b000c82d9ac6a47
Muhydeen Adebayo <adediranma@examroom.ai>
2023-08-13 13:16:51 +0100

    Api error

---
+++ HttpClientDemo/Helpers/ApiErrorHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+
+namespace HttpClientDemo.Helpers
+{
+    public class ApiErrorHandler
+    {
+        public static Dictionary<string, List<string>> GetApiErrorResponse(string responseBody)
+        {
+            var result = new Dictionary<string, List<string>>();
+
+            var element = JsonSerializer.Deserialize<JsonElement>(responseBody);
+            var JsonError = element.GetProperty("error");
+
+            foreach (var item in JsonError.EnumerateObject())
+            {
+                var errorList = new List<string>();
+                var key = item.Name;
+                foreach(var x in item.Value.EnumerateObject())
+                {
+                    errorList.Add(x.ToString());
+                }
+                result.Add(key, errorList);
+            }
+
+            return result;
+        }
+    }
+}
