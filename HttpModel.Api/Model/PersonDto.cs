2569c07835d7c6b1f13e7efd29c7830966e155a1
Muhydeen Adebayo <adediranma@examroom.ai>
2023-08-15 15:22:10 +0100

    Try and Catch implemented

--- HttpModel.Api/Model/PersonDto.cs
+++ HttpModel.Api/Model/PersonDto.cs
@@ -1,12 +1,15 @@
 ﻿#nullable disable
+using System.ComponentModel.DataAnnotations;
+
 namespace HttpModel.Api.Model
 {
     public class PersonDto
     {
+        [Required]
         public string Name { get; set; }
-        
+        [Required, Phone]
         public string PhoneNumber { get; set; }
-        
+        [Required, EmailAddress]
         public string Email { get; set; }
     }
 }
