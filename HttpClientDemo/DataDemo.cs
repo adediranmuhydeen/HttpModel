de3d69137d324a9757f660d9176415f6b6b6573e
Muhydeen Adebayo <adediranma@examroom.ai>
2023-09-13 12:46:52 +0100

    Serilog

--- HttpClientDemo/DataDemo.cs
+++ HttpClientDemo/DataDemo.cs
@@ -1,4 +1,6 @@
 ﻿using HttpClientDemo.Helpers;
+using Microsoft.Extensions.Logging;
+using Serilog;
 using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json;
@@ -6,8 +8,9 @@ using System.Text.Json;
 namespace HttpClientDemo
 {
 #nullable disable
-    public static class DataDemo<T> where T : class
+    public  class DataDemo<T> where T : class
     {
+
         public static async Task<T> HttpGet(string uri)
         {
             using (var client = new HttpClient())
@@ -37,15 +40,22 @@ namespace HttpClientDemo
 
         public static async Task<string> HttpPostAsync(string uri, T data)
         {
-            using(var client = new HttpClient())
+            try
             {
-                var neResponse = JsonSerializer.Serialize(data);
-                var stringContent = new StringContent(neResponse, Encoding.UTF8, "application/json");
-                var response = await client.PostAsync(uri, stringContent);
-                var result = await response.Content.ReadAsStringAsync();
-                //ApiErrorHandler<T>.GetApiErrorResponse(result);
-                return result;
+                using (var client = new HttpClient())
+                {
+                    var neResponse = JsonSerializer.Serialize(data);
+                    var stringContent = new StringContent(neResponse, Encoding.UTF8, "application/json");
+                    var response = await client.PostAsync(uri, stringContent);
+                    var result = await response.Content.ReadAsStringAsync();
+                    return result;
+                }
             }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message, ex);
+            }
+            return null;
         }
 
         public static async Task<T> GetOneEntity(string uri, int id)
@@ -67,21 +77,9 @@ namespace HttpClientDemo
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Error: {ex.InnerException}");
+                Log.Error(ex.Message, ex);
             }
             return null;
         }
-    }
-
-    public class Res
-    {
-        public List<Data> Data{ get; set; }
-    }
-
-    public class Data
-    {
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Publication_Date { get; set; }
     }
 }
